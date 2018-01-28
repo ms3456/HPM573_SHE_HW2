2a6215921c4e93ca86d6728dbb896779961c38c2
Meng She <mengshe@Mengs-MacBook.local>
2018-01-27 20:41:02 -0500

    Initial commit

---
+++ HUI3.py
@@ -0,0 +1,49 @@
+Constant = 0.371
+N6 = 1.371
+
+dictCoefficients = {'Vision': [1.00, 0.98, 0.89, 0.84, 0.75, 0.61],
+                    'Hearing': [1.00, 0.95, 0.89, 0.80, 0.74, 0.61],
+                    'Speech': [1.00, 0.94, 0.89, 0.81, 0.68],
+                    'Ambulation': [1.00, 0.93, 0.86, 0.73, 0.65, 0.58],
+                    'Dexterity': [1.00, 0.95, 0.88, 0.76, 0.65, 0.56],
+                    "Emotion": [1.00, 0.95, 0.85, 0.64, 0.46],
+                    'Cognition': [1.00, 0.92, 0.95, 0.83, 0.60, 0.42],
+                    'Pain': [1.00, 0.96, 0.90, 0.77, 0.55]};
+
+def get_score(vision, hearing, speech, ambulation, dexterity, emotion, cognition, pain):
+    """
+    :param vision: level of vision
+    :param hearing: level of hearing
+    :param speech: level of speech
+    :param ambulation: level of ambulation
+    :param dexterity: level of dexterity
+    :param emotion: level of emotion
+    :param cognition: level of cognition
+    :param pain: level of pain
+    :return:
+    """
+
+    # check entered values for validity
+    if not (vision in [1, 2, 3, 4, 5, 6]):
+        raise ValueError ('Vision level can only take 1, 2, 3, 4, 5 or 6')
+    if not (hearing in [1, 2, 3, 4, 5, 6]):
+        raise ValueError ('Hearing level can only take 1, 2, 3, 4, 5 or 6')
+    if not (speech in [1, 2, 3, 4, 5]):
+        raise ValueError ('Speech level can only take 1, 2, 3, 4 or 5')
+    if not (ambulation in [1, 2, 3, 4, 5, 6]):
+        raise ValueError ('Ambulation level can only take 1, 2, 3, 4, 5 or 6')
+    if not (dexterity in [1, 2, 3, 4, 5, 6]):
+        raise ValueError ('Dexterity level can only take 1, 2, 3, 4, 5 or 6')
+    if not (emotion in [1, 2, 3, 4, 5]):
+        raise ValueError ('Emotion level can only take 1, 2, 3, 4 or 5')
+    if not (cognition in [1, 2, 3, 4, 5, 6]):
+        raise ValueError ('Cognition level can only take 1, 2, 3, 4, 5 or 6')
+    if not (pain in [1, 2, 3, 4, 5]):
+        raise ValueError ('Pain level can only take 1, 2, 3, 4 or 5')
+
+    score = N6*dictCoefficients['Vision'][vision-1]*dictCoefficients['Hearing'][hearing-1]*\
+            dictCoefficients['Speech'][speech-1]*dictCoefficients['Ambulation'][ambulation-1]*\
+            dictCoefficients['Dexterity'][dexterity-1]*dictCoefficients['Emotion'][emotion-1]*\
+            dictCoefficients['Cognition'][cognition-1]*dictCoefficients['Pain'][pain-1] - Constant
+
+    return score
---
+++ UseHUI3.py
@@ -0,0 +1,4 @@
+import HUI3 as hui3
+
+print(hui3.get_score(1,1,1,1,1,1,1,7))
+
